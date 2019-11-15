1d59fc9cbc89b6d049cbe19623914507bbf267b6
YSJ <theblesedcity@hanmail.net>
2019-11-16 01:24:10 +0900

    ENH : model admin에서 text 보이게 수장

--- bookmark/models.py
+++ bookmark/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Bookmark(models.Model):
     bookmark_name = models.CharField(max_length=100)
     bookmark_url = models.URLField('Site URL')
+
+    def __str__(self):
+        return self.bookmark_name
